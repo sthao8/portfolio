27882f312cf30bae4f9fe4defe9bd8b6bd0db45d
Shoua Thao <s.thao008@gmail.com>
2024-09-09 01:47:53 +0200

    All features implemented. TODO fix text

--- app.py
+++ app.py
@@ -5,22 +5,86 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    image="/static/lotus.jpg"
+    image = "/static/image.jpg"
+    skills = [
+        {
+            "name": "Python",
+            "icon": "bxl-python"
+        },
+        {
+            "name": "HTML",
+            "icon": "bxl-html5"
+        },
+        {
+            "name": "CSS",
+            "icon": "bxl-css3"
+        },
+        {
+            "name": "Javascript",
+            "icon": "bxl-javascript"
+        },
+        {
+            "name": "MySQL",
+            "icon": "bxs-data"
+        },
+        {
+            "name": "Flask",
+            "icon": "bxl-flask"
+        },
+        {
+            "name": "AI/ML",
+            "icon": "bx-math"
+        },
+        {
+            "name": "Bootstrap",
+            "icon": "bxl-bootstrap"
+        },
+        {
+            "name": "C",
+            "icon": "bx-code-curly"
+        }
+    ]
     projects = [
         {"name": "Bank App",
-         "description": "stuff",
-         "url": "https://bank-sthao8.pythonanywhere.com"},
+         "description": "Bank app built with Bootstrap, Flask, with seeded customers in customizable locales supported by Faker stored in a relational MySQL database.",
+         "url": "https://bank-sthao8.pythonanywhere.com",
+         "source_code": "https://github.com/sthao8/bank"
+         },
         {"name": "Hotel Booking System",
-         "description": "more stuff",
-         "url": "#"},
+         "description": "Hotel booking system built with Bootstrap, Flask in Python with data stored in MySQL relational databases.",
+         "url": None,
+         "source_code": "https://github.com/sthao8/BluePrintsHotel"
+         },
          {"name": "Plant Journal",
-         "description": "plant journal description",
-         "url": "https://plantjournal-sthao8.pythonanywhere.com"},
+         "description": "My first project made with Flask written in Python! A cute plant journal with data stored in SQLite databases, feature plant search via API lookup!",
+         "url": None, #"https://plantjournal-sthao8.pythonanywhere.com",
+         "source_code": "https://github.com/sthao8/plant_journal"
+         },
          {"name": "Portfolio",
-         "description": "You're visiting this page, silly!",
-         "url": "#"}
+         "description": "A responsive portfolio coded by Shoua Thao with Flask and styled by Bootstrap.",
+         "url": None,
+         "source_code": "https://github.com/sthao8/portfolio"
+         }
     ]
-    about_me_text = "Hello, world! I'm a student at Teknikhögskolan studying Python with a focus in AI looking for a LIA-internship for weeks 40-47."
+    links = [
+        {
+            "url": "https://www.linkedin.com/in/shoua-thao-7986512bb/",
+            "icon": "bxl-linkedin",
+            "title": "LinkedIn"
+        },
+        {
+            "url": "https://github.com/sthao8",
+            "icon": "bxl-github",
+            "title": "Github"
+        },
+        {
+            "url": "/static/Shoua_Thao_CV_ENG.pdf",
+            "icon": "bxs-file",
+            "title": "CV"
+        }
+    ]
+
+    about_me_text = "Energized by finding solutions to problems, solving puzzles, and lifelong learning, I make carefully crafted and responsive apps with an eye for design."
     email = "shoua.thao@studerande.plushogskolan.se"
     
     return render_template(
@@ -28,7 +92,9 @@ def index():
         image=image,
         projects=projects,
         about_me_text=about_me_text,
-        email=email)
+        email=email,
+        skills=skills,
+        links=links)
 
 if __name__ == "__main__":
     app.run(debug=True)
